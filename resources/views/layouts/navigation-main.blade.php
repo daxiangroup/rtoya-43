1553dd84919051fef276ae25b0377a4782f90a85
tylers <tylers@nationalfibre.net>
2014-09-15 23:23:58 -0400

    Added Artists to the main navigation

--- resources/views/layouts/navigation-main.blade.php
+++ resources/views/layouts/navigation-main.blade.php
@@ -4,9 +4,11 @@
         <li>{!! HTML::linkRoute('dashboard', 'Dashboard') !!}</li>
         <li>{!! HTML::linkRoute('account', 'Account Settings') !!}</li>
         <li>{!! HTML::linkRoute('art', 'Arts') !!}</li>
+        <li>{!! HTML::linkRoute('artist.index', 'Artists') !!}</li>
         <li>{!! HTML::linkRoute('signin.signout', 'Sign Out') !!}</li>
         @else
         <li>{!! HTML::linkRoute('art', 'Arts') !!}</li>
+        <li>{!! HTML::linkRoute('artist.index', 'Artists') !!}</li>
         <li>{!! HTML::linkRoute('signin', 'Sign In') !!}</li>
         @endif
     </ul>
